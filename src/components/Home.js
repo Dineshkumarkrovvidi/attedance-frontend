b6a2be9111c35dc5936390e072d1df7113f1a1d3
Dinesh Kumar <dineshkumarkrovvidi@gmail.com>
2026-03-17 15:10:52 +0530

    filenames changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function Main() {
+function Home() {
   const navigate = useNavigate();
 
   const handleNavigate = (path) => {
